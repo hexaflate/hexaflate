777230b82ae26e99a5c673ab68dd89fde2fdce18
riskirills66 <riskirills66@gmail.com>
2026-05-06 23:34:33 +0700

    add favicon

--- updater/build.rs
+++ updater/build.rs
@@ -2,8 +2,7 @@ fn main() {
     #[cfg(windows)]
     {
         let mut res = winres::WindowsResource::new();
-        // Shell32.dll icon index 47 = the classic Windows Update/settings gear icon
-        res.set_icon_with_id("C:\\Windows\\System32\\shell32.dll", "47");
+        res.set_icon("assets/icon.ico");
         res.compile().unwrap();
     }
 }
